cffda950fbac6e823d84a7fe5fbb4308d5df9ac1
ArMor <73475228+Uchiha110@users.noreply.github.com>
2021-12-06 18:17:52 +0300

    Create mainSettings.py

---
+++ mainSettings.py
@@ -0,0 +1,13 @@
+# settings
+url = 'https://sochi.cian.ru/kupit-kvartiru-novostroyki/'
+host = 'https://sochi.cian.ru/'
+payload = {
+    'accept': '*/*',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+}
+# bot settings
+APIToken = 'yourBotFatherToken'
+
+# FeedBack
+Telegram = 'https://t.me/ArMor_0001'
+Discord = 'Ar.Mor.#2906'
